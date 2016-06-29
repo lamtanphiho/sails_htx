aa94052ef85e71dedc5ebd6b7621c86c75e833d0
tai <lamtanphiho1@gmail.com>
2016-06-30 01:54:10 +0700

    dsds

--- config/connections.js
+++ config/connections.js
@@ -44,7 +44,7 @@ module.exports.connections = {
     adapter: 'sails-mysql',
     host: 'localhost',
     user: 'root', //optional
-    password: '', //optional
+    password: 'Betapp!@#$188', //optional
     database: 'sailshtx' //optional
   },
 
